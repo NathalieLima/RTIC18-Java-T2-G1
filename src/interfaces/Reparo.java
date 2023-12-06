45cde9b2c8beb871b773e15a3ffbeb03d3ae6c79
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2023-12-06 19:04:49 -0300

    feat:implementando a classe reparo

---
+++ src/interfaces/Reparo.java
@@ -0,0 +1,59 @@
+package interfaces;
+
+public class Reparo {
+    String descricao;
+    String previsao;
+    String dataInicio;
+    String dataFim;
+    boolean resolvido;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPrevisao() {
+        return previsao;
+    }
+
+    public void setPrevisao(String previsao) {
+        this.previsao = previsao;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(String dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public boolean isResolvido() {
+        return resolvido;
+    }
+
+    public void setResolvido(boolean resolvido) {
+        this.resolvido = resolvido;
+    }
+
+    public Reparo(String descricao, String previsao, String dataInicio, String dataFim) {
+        this.descricao = descricao;
+        this.previsao = previsao;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.resolvido = false;
+    }
+
+
+}
